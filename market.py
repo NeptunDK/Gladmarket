c989a219825c13bf09fa11a830dfbe4d4004fe1e
NeptunDK <pallenda@gmail.com>
2017-06-28 23:28:01 +0200

    Stock.py is starting to work nicely. Place order method support instant buy/sell, or add_order_to_escrow method. Market.py is a mess, still pondering if process_orders, modify_order should be. Today I'm leaning to putting them in the stock class.

--- market.py
+++ market.py
@@ -12,4 +12,77 @@ class Market:
         self.stocks = {}
 
     # todo process all stocks
-    #
+
+    def process_orders(self):
+        # todo still unsure if this should be here or in the stock class?
+        # todo FIFO
+
+        # todo loop to match if there is buy orders matching
+        for order in self.orders['buy']:
+            if order.price >= self.price:
+                logging.warning(f"{order} matched price of: {self.price}")
+                # todo update player credit/portfolio
+                self.remove_order(order)
+
+        # todo loop to match if there is sell orders matching
+        for order in self.orders['sell']:
+            if order.price <= self.price:
+                logging.warning(f"{order} matched price of: {self.price}")
+                # todo update player credit/portfolio
+                self.remove_order(order)
+
+        # todo match buy with sell orders
+
+
+    def remove_order(self, order):
+        # todo alter to work with process_orders / complete orders in market class
+        if order in self.orders[order.order_type]:
+            self.orders[order.order_type].remove(order)
+            # todo if buy order, require full buy amount to be put in escrow refund money to player
+            # todo if sell order, return stocks to player's portfolio
+            logging.warning(f"{order} removed.")
+        else:
+            logging.warning(f"{order} not found!")
+
+
+    def modify_order(self, oldorder, neworder):
+        # todo player modify order, not sure yet if this is needed or if there is a better way
+        # todo unittest
+        # order exsist?
+        # remove old order
+        # add new order
+        pass
+
+class TestStock(unittest.TestCase):
+    def setUp(self):
+        self.teststock = Stock('testStock', 1000)
+        # price, volume, player, type
+        self.buyorder = Order(100, 1, 'NPC', 'buy')
+        self.sellorder = Order(100, 1, 'NPC', 'sell')
+
+    def test_process_orders(self):
+        # todo add Player class instance, to test credit/portfolio updates,
+        # or maybe have that part in a different test/class
+        teststock = Stock('testStock', 1000)
+        self.assertEqual(teststock.price, 1000)
+        buyorder = Order(900, 1, 'NPC', 'buy')
+        sellorder = Order(1100, 1, 'NPC', 'sell')
+        teststock.add_order_to_escrow(buyorder)
+        teststock.add_order_to_escrow(sellorder)
+        teststock.process_orders()
+        self.assertIn(buyorder, teststock.orders['buy'])
+        self.assertIn(sellorder, teststock.orders['sell'])
+        teststock.price = 900
+        self.assertEqual(teststock.price, 900)
+        teststock.process_orders()
+        self.assertNotIn(buyorder, teststock.orders['buy'])
+        self.assertIn(sellorder, teststock.orders['sell'])
+        teststock.price = 1100
+        self.assertEqual(teststock.price, 1100)
+        teststock.process_orders()
+        self.assertNotIn(buyorder, teststock.orders['buy'])
+        self.assertNotIn(sellorder, teststock.orders['sell'])
+        print('test_process_orders passed.')
+
+if __name__ == '__main__':
+    unittest.main()
--- stock.py
+++ stock.py
@@ -14,17 +14,19 @@ class Stock:  # could also be called asset since it could be more than stocks, r
         self.orders = {'buy': [], 'sell': []}  # hmmm set or list????
         # todo orderID, added to Order namedtuple?
 
-    def ranwalk_price(self):
-        # todo not sure yet if it should be here or in the market class
-        # todo unittest
-        pass
+    def place_order(self, order):
+        if order.order_type == 'buy' and self.price <= order.price:
+            logging.warning(f"{order.player} bought {order.vol} shares of {self.name} for a price of {order.vol * order.price}.")
+            logging.warning(f"{order.player} completed {order}.")
+            return True
+        elif order.order_type == 'sell' and self.price >= order.price:
+            logging.warning(f"{order.player} sold {order.vol} shares of {self.name} for a price of {order.vol * order.price}.")
+            logging.warning(f"{order.player} completed {order}.")
+            return True
+        else:
+            self.add_order_to_escrow(order)
 
-    def add_order(self, neworder):
-        # todo on buy check if offer price is higher than current price = instant buy
-        # todo      if lower price put in buy_orders
-
-        # todo on sell check if offer price is lower than current price = instant sell
-        # todo      if higher put in sell_orders
+    def add_order_to_escrow(self, neworder):
         similar_order = self.list_similar_player_order(neworder)
 
         if not similar_order:
@@ -50,49 +52,22 @@ class Stock:  # could also be called asset since it could be more than stocks, r
     def list_player_sell_orders(self, player):
         return [match for match in self.orders['sell'] if match.player == player]
 
-    def remove_order(self, order):
-        # todo alter to work with process_orders / complete orders in market class
-        if order in self.orders[order.order_type]:
-            self.orders[order.order_type].remove(order)
-            # todo if buy order, require full buy amount to be put in escrow refund money to player
-            # todo if sell order, return stocks to player's portfolio
-            logging.warning(f"{order} removed.")
-        else:
-            logging.warning(f"{order} not found!")
+    def list_buy_orders(self):
+        return self.orders['buy']
+
+    def list_sell_orders(self):
+        return self.orders['sell']
 
     def cancel_order(self, order):
-        # todo how to return ok/fail to user?
+        # todo how to return ok/fail to user ok to just return True/False
+        # todo order id
         if order in self.orders[order.order_type]:
             self.orders[order.order_type].remove(order)
             logging.warning(f"{order} cancelled.")
+            return True
         else:
             logging.warning(f"{order} not found!")
-
-    def modify_order(self, oldorder, neworder):
-        # todo player modify order, not sure yet if this is needed or if there is a better way
-        # todo unittest
-        # order exsist?
-        # remove old order
-        # add new order
-        pass
-
-    def process_orders(self):
-        # todo move to market?
-        # todo loop to match if there is buy orders matching
-        for order in self.orders['buy']:
-            if order.price >= self.price:
-                logging.warning(f"{order} matched price of: {self.price}")
-                # todo update player credit/portfolio
-                self.remove_order(order)
-
-        # todo loop to match if there is sell orders matching
-        for order in self.orders['sell']:
-            if order.price <= self.price:
-                logging.warning(f"{order} matched price of: {self.price}")
-                # todo update player credit/portfolio
-                self.remove_order(order)
-
-        # todo match buy with sell orders?
+            return False
 
     def update_low_high_price(self):
         if self.price > self.high_price:
@@ -100,15 +75,19 @@ class Stock:  # could also be called asset since it could be more than stocks, r
         if self.price < self.low_price:
             self.low_price = self.price
 
+    def ranwalk_price(self):
+        # todo not sure yet if it should be here or in the market class
+        # todo unittest
+        pass
+
+    # todo if price == 0 -> out of buisness -> remove from market? Might be more suited in the Market class.
+    # todo stock price history?
+    # todo logging?
+
     # todo split, only needed if I implement limited number of shares?
     # todo split order method needed? maybe not if buy orders always take the player credit in escrow
     # might only be needed if there is support for buy under current price or sell over current price between players
     # or if there is a limited supply/demand
-
-    # todo stock price history?
-    # todo logging?
-
-    # todo if price == 0 -> out of buisness -> remove from market? Might be more suited in the Market class.
 
 
 class TestStock(unittest.TestCase):
@@ -116,31 +95,9 @@ class TestStock(unittest.TestCase):
         self.teststock = Stock('testStock', 1000)
         # price, volume, player, type
         self.buyorder = Order(100, 1, 'NPC', 'buy')
+        self.buyorder_two = Order(1100, 1, 'NPC', 'buy')
         self.sellorder = Order(100, 1, 'NPC', 'sell')
-
-    def test_process_orders(self):
-        # todo add Player class instance, to test credit/portfolio updates,
-        # or maybe have that part in a different test/class
-        teststock = Stock('testStock', 1000)
-        self.assertEqual(teststock.price, 1000)
-        buyorder = Order(900, 1, 'NPC', 'buy')
-        sellorder = Order(1100, 1, 'NPC', 'sell')
-        teststock.add_order(buyorder)
-        teststock.add_order(sellorder)
-        teststock.process_orders()
-        self.assertIn(buyorder, teststock.orders['buy'])
-        self.assertIn(sellorder, teststock.orders['sell'])
-        teststock.price = 900
-        self.assertEqual(teststock.price, 900)
-        teststock.process_orders()
-        self.assertNotIn(buyorder, teststock.orders['buy'])
-        self.assertIn(sellorder, teststock.orders['sell'])
-        teststock.price = 1100
-        self.assertEqual(teststock.price, 1100)
-        teststock.process_orders()
-        self.assertNotIn(buyorder, teststock.orders['buy'])
-        self.assertNotIn(sellorder, teststock.orders['sell'])
-        print('test_process_orders passed.')
+        self.sellorder_two = Order(1100, 1, 'NPC', 'sell')
 
     def test_create_stock(self):
         self.assertEqual(self.teststock.name, 'testStock') and self.assertEqual(self.teststock.price, 1000)
@@ -152,12 +109,26 @@ class TestStock(unittest.TestCase):
         self.assertEqual(self.teststock.price, 1200)
         print('test_alterprice passed.')
 
+    def test_place_order_buy(self):
+        self.assertTrue(self.teststock.place_order(Order(1000, 1, 'NPC', 'buy')))
+        self.assertTrue(self.teststock.place_order(Order(1100, 1, 'NPC', 'buy')))
+        self.assertFalse(self.teststock.place_order(Order(100, 1, 'NPC', 'buy')))
+        self.assertIn(Order(100, 1, 'NPC', 'buy'), self.teststock.list_buy_orders())
+        print('test_place_order_buy passed.')
+
+    def test_place_order_sell(self):
+        self.assertTrue(self.teststock.place_order(Order(1000, 1, 'NPC', 'sell')))
+        self.assertTrue(self.teststock.place_order(Order(100, 1, 'NPC', 'sell')))
+        self.assertFalse(self.teststock.place_order(Order(1100, 1, 'NPC', 'sell')))
+        self.assertIn(Order(1100, 1, 'NPC', 'sell'), self.teststock.list_sell_orders())
+        print('test_place_order_sell passed.')
+
     def test_add_order(self):
         # setup
-        self.teststock.add_order(Order(100, 5, 'NPC', 'buy'))
-        self.teststock.add_order(Order(200, 5, 'NPC', 'buy'))
-        self.teststock.add_order(Order(100, 5, 'Neptun', 'buy'))
-        self.teststock.add_order(Order(100, 1, 'NPC', 'buy'))
+        self.teststock.add_order_to_escrow(Order(100, 5, 'NPC', 'buy'))
+        self.teststock.add_order_to_escrow(Order(200, 5, 'NPC', 'buy'))
+        self.teststock.add_order_to_escrow(Order(100, 5, 'Neptun', 'buy'))
+        self.teststock.add_order_to_escrow(Order(100, 1, 'NPC', 'buy'))
         # checks
         self.valid_new_order = Order(100, 6, 'NPC', 'buy')
         self.assertIn(self.valid_new_order, self.teststock.orders['buy'])
@@ -168,26 +139,26 @@ class TestStock(unittest.TestCase):
         print('test_add_order passed.')
 
     def test_add_buy_order(self):
-        self.teststock.add_order(self.buyorder)
+        self.teststock.add_order_to_escrow(self.buyorder)
         self.assertIn(self.buyorder, self.teststock.orders['buy'])
         print('test_add_buy_order passed.')
 
     def test_add_sell_order(self):
-        self.teststock.add_order(self.sellorder)
+        self.teststock.add_order_to_escrow(self.sellorder)
         self.assertIn(self.sellorder, self.teststock.orders['sell'])
         print('test_add_sell_order passed.')
 
     def test_list_player_buy_orders(self):
-        self.teststock.add_order(self.sellorder)
+        self.teststock.add_order_to_escrow(self.sellorder)
         self.assertNotIn(self.sellorder, self.teststock.list_player_buy_orders(self.sellorder.player))
-        self.teststock.add_order(self.buyorder)
+        self.teststock.add_order_to_escrow(self.buyorder)
         self.assertIn(self.buyorder, self.teststock.list_player_buy_orders(self.buyorder.player))
         print('test_list_player_buy_orders passed.')
 
     def test_list_player_sell_orders(self):
-        self.teststock.add_order(self.buyorder)
+        self.teststock.add_order_to_escrow(self.buyorder)
         self.assertNotIn(self.buyorder, self.teststock.list_player_sell_orders(self.sellorder.player))
-        self.teststock.add_order(self.sellorder)
+        self.teststock.add_order_to_escrow(self.sellorder)
         self.assertIn(self.sellorder, self.teststock.list_player_sell_orders(self.buyorder.player))
         print('test_list_player_buy_orders passed.')
 
@@ -196,26 +167,26 @@ class TestStock(unittest.TestCase):
         existing_order2 = Order(200, 5, 'NPC', 'buy')
         existing_order3 = Order(100, 5, 'Neptun', 'buy')
         new_order = Order(100, 1, 'NPC', 'buy')
-        self.teststock.add_order(existing_order)
+        self.teststock.add_order_to_escrow(existing_order)
         self.assertIn(existing_order, self.teststock.orders['buy'])
-        self.teststock.add_order(existing_order2)
+        self.teststock.add_order_to_escrow(existing_order2)
         self.assertIn(existing_order2, self.teststock.orders['buy'])
-        self.teststock.add_order(existing_order3)
+        self.teststock.add_order_to_escrow(existing_order3)
         self.assertIn(existing_order3, self.teststock.orders['buy'])
         self.assertIn(existing_order, self.teststock.list_similar_player_order(new_order))
         self.assertNotIn(existing_order3, self.teststock.list_similar_player_order(new_order))
         print('test_player_similar_buy_order passed.')
 
     def test_add_duplicate_buy_order(self):
-        self.teststock.add_order(self.buyorder)
-        self.teststock.add_order(self.buyorder)
+        self.teststock.add_order_to_escrow(self.buyorder)
+        self.teststock.add_order_to_escrow(self.buyorder)
         duplicate_order = Order(100, 2, 'NPC', 'buy')
         self.assertIn(duplicate_order, self.teststock.orders['buy'])
         self.assertTrue(len(self.teststock.orders['buy']) == 1)
         print('test_add_duplicate_buy_orders passed.')
 
     def test_cancel_order(self):
-        self.teststock.add_order(self.buyorder)
+        self.teststock.add_order_to_escrow(self.buyorder)
         self.assertIn(self.buyorder, self.teststock.orders['buy'])
         self.teststock.cancel_order(self.buyorder)
         self.assertNotIn(self.buyorder, self.teststock.orders['buy'])
@@ -223,10 +194,9 @@ class TestStock(unittest.TestCase):
         print('test_cancel_order passed.')
 
     def test_add_1337_similar_orders(self):
-        # todo need updating to new namedtuple format
         bigorder = Order(100, 1337, 'NPC', 'buy')
         for i in range(1337):
-            self.teststock.add_order(self.buyorder)
+            self.teststock.add_order_to_escrow(self.buyorder)
         self.assertIn(bigorder, self.teststock.orders['buy'])
         self.assertEqual(len(self.teststock.orders['buy']), 1)
         print('test_add_1337_similar_orders passed.')
